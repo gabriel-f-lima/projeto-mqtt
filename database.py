1e44c5118b9de4a0b4d0dabc3f6477b6ae723691
Gabriel <gabriel.f.lima21@aluno.senai.br>
2026-09-18 09:28:20 -0300

    Refactor database connection management
    Refactored database connection handling by creating a new function to obtain active database connections. Updated the insert and fetch functions to use this new connection method.

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 import os
 import psycopg2
-from psycopg2 import pool
 from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 
@@ -8,21 +7,38 @@ load_dotenv()
 
 URL_BANCO_DADOS = os.getenv("DATABASE_URL")
 
-db_pool = psycopg2.pool.SimpleConnectionPool(1, 10, URL_BANCO_DADOS)
+def obter_conexao_bd():
+    """Cria e retorna a conexão ativa com o banco de dados Neon."""
+    return psycopg2.connect(URL_BANCO_DADOS)
 
 def inserir_leitura_sensor(id_dispositivo, distancia_cm, esta_detectado=False):
-    conexao = db_pool.getconn()
-    try:
-        cursor = conexao.cursor()
-        cursor.execute(
-            """
-            INSERT INTO sensor_readings (device_id, distance_cm, is_detected)
-            VALUES (%s, %s, %s);
-            """,
-            (id_dispositivo, distancia_cm, esta_detectado)
-        )
-        conexao.commit()
-        cursor.close()
-    finally:
-        # Return connection to the pool instead of closing it entirely
-        db_pool.putconn(conexao)
+    """Insere o registro de uma leitura no banco de dados."""
+    conexao = obter_conexao_bd()
+    cursor = conexao.cursor()
+    
+    cursor.execute(
+        """
+        INSERT INTO sensor_readings (device_id, distance_cm, is_detected)
+        VALUES (%s, %s, %s);
+        """,
+        (id_dispositivo, distancia_cm, esta_detectado)
+    )
+    
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+
+def buscar_leituras_sensor(limite=50):
+    """Busca as leituras mais recentes do banco formatadas como dicionário."""
+    conexao = obter_conexao_bd()
+    cursor = conexao.cursor(cursor_factory=RealDictCursor)
+    
+    cursor.execute(
+        "SELECT * FROM sensor_readings ORDER BY measured_at DESC LIMIT %s;", 
+        (limite,)
+    )
+    leituras = cursor.fetchall()
+    
+    cursor.close()
+    conexao.close()
+    return leituras
